4e6baac3d3a345525e44123ae593736103a2e694
bernardomrl <bernardomrl@icloud.com>
2023-09-11 20:34:55 -0300

    Alterado navigation.ts para dentro do index.ts.

--- app/src/components/landing-page/index.ts
+++ app/src/components/landing-page/index.ts
@@ -1,6 +1,21 @@
-export { default as navigation } from '@/components/landing-page/navigation';
 export { default as Header } from '@/components/landing-page/Header';
 export { default as Hero } from '@/components/landing-page/Hero';
 export { default as Feature } from '@/components/landing-page/Feature';
 export { default as Pricing } from '@/components/landing-page/Pricing';
 export { default as Footer } from '@/components/landing-page/Footer';
+
+import {
+  InformationCircleIcon,
+  ShieldCheckIcon,
+  RectangleStackIcon,
+  UserIcon,
+  UserPlusIcon
+} from '@heroicons/react/20/solid';
+
+export const navigation = [
+  { name: 'Início', anchor: '#about', icon: InformationCircleIcon },
+  { name: 'Segurança', anchor: '#feature', icon: ShieldCheckIcon },
+  { name: 'Planos', anchor: '#pricing', icon: RectangleStackIcon },
+  { name: 'Login', anchor: '/auth/signin', icon: UserIcon },
+  { name: 'Cadastro', anchor: '/auth/signup', icon: UserPlusIcon }
+];
