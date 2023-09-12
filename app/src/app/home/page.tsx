01fd643835843579607119966b97cd7da87f73db
Bernardo Meirelles <111180963+bernardomrl@users.noreply.github.com>
2023-09-12 11:08:38 +0000

    Alterado componente Search para dentro da page Home

--- app/src/app/home/page.tsx
+++ app/src/app/home/page.tsx
@@ -1,10 +1,10 @@
 import { Header } from '@/components';
-import { Search } from '@/components/home';
 
 import {
   HomeIcon,
   ArrowLeftOnRectangleIcon,
-  Cog6ToothIcon
+  Cog6ToothIcon,
+  MagnifyingGlassIcon
 } from '@heroicons/react/24/outline';
 
 export default function Home() {
@@ -14,7 +14,27 @@ export default function Home() {
       <div className="drawer-content flex flex-col">
         <Header />
         {/* Page content here */}
-        <Search />
+        {/* Search bar*/}
+        <div className="w-full h-screen flex flex-col justify-center items-center bg-base-100 space-y-8">
+          <h1 className="text-2xl font-bold font-poppins">
+            Pesquisar freelancers
+          </h1>
+          <div className="w-full flex flex-col max-w-md space-y-2">
+            <div className="relative">
+              <input
+                type="text"
+                name="password"
+                className="input input-md bg-base-200 w-full pr-14"
+              />
+              <button
+                className="absolute inset-y-0 right-0 flex items-center p-3 btn btn-ghost"
+                type="button"
+              >
+                <MagnifyingGlassIcon className="h-5 w-5" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="drawer-side">
         <label htmlFor="home-drawer" className="drawer-overlay"></label>
