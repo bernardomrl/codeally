c3994d4be738c0b6aebb3692e21540a3a9509714
Bernardo Meirelles <111180963+bernardomrl@users.noreply.github.com>
2023-09-12 11:09:42 +0000

    Corrigido erros de estilização

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -5,7 +5,7 @@ import { Bars3BottomLeftIcon } from '@heroicons/react/24/outline';
 
 export default function Header() {
   return (
-    <div className="navbar absolute top-0 w-full xl:w-[calc(100vw-20rem)] bg-base-200/50 flex justify-between">
+    <div className="navbar absolute top-0 w-full xl:w-[calc(100vw-20rem)] bg-base-200 flex justify-between z-50">
       <div className="flex-none">
         <label htmlFor="home-drawer" className="btn btn-ghost xl:hidden">
           <Bars3BottomLeftIcon className="inline-block w-6 h-6 stroke-current" />
