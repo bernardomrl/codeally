d5454fd71b9fcdd284bbe00b85b819af917aadf9
bernardomrl <bernardomrl@icloud.com>
2023-09-11 22:18:40 -0300

    Adicionado componente Header

---
+++ app/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+import DefaultUserImg from '@/assets/img/profile.png';
+import { Bars3BottomLeftIcon } from '@heroicons/react/24/outline';
+
+export default function Header() {
+  return (
+    <div className="navbar absolute top-0 w-full xl:w-[calc(100vw-20rem)] bg-base-200/50 flex justify-between">
+      <div className="flex-none">
+        <label htmlFor="home-drawer" className="btn btn-ghost xl:hidden">
+          <Bars3BottomLeftIcon className="inline-block w-6 h-6 stroke-current" />
+        </label>
+      </div>
+      <div>
+        <a
+          href="/profile"
+          tabIndex={0}
+          className="btn btn-ghost btn-circle avatar"
+        >
+          <div className="w-10 rounded-full">
+            <Image src={DefaultUserImg} alt="Avatar" width={200} height={200} />
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as CustomToaster } from '@/components/CustomToaster';
+export { default as Header } from '@/components/Header';
