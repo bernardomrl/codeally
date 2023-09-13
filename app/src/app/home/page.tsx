54d08523e9ecb583c981f45d918beffb39cc4d64
Bernardo Meirelles <111180963+bernardomrl@users.noreply.github.com>
2023-09-13 19:17:50 +0000

    Alterado filtragem para competências.

--- app/src/app/home/page.tsx
+++ app/src/app/home/page.tsx
@@ -6,24 +6,28 @@ import { Header } from '@/components';
 import { Combobox, Transition } from '@headlessui/react';
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
-const people = [
-  { id: 1, name: 'Wade Cooper' },
-  { id: 2, name: 'Arlene Mccoy' },
-  { id: 3, name: 'Devon Webb' },
-  { id: 4, name: 'Tom Cook' },
-  { id: 5, name: 'Tanya Fox' },
-  { id: 6, name: 'Hellen Schmidt' }
+const competences = [
+  { id: 1, name: 'Desenvolvedor Front-End', value: 'DEVFE' },
+  { id: 2, name: 'Desenvolvedor Back-End', value: 'DEVBE' },
+  { id: 3, name: 'Desenvolvedor FullStack', value: 'DEVFS' },
+  { id: 4, name: 'Desenvolvedor Mobile', value: 'DEVMB' }
 ];
 
+interface Competence {
+  id: number;
+  name: string;
+  value: string;
+}
+
 export default function Home() {
-  const [selected, setSelected] = useState(people[0]);
+  const [selected, setSelected] = useState(competences[0]);
   const [query, setQuery] = useState('');
 
-  const filteredPeople =
+  const filteredSearch =
     query === ''
-      ? people
-      : people.filter((person) =>
-          person.name
+      ? competences
+      : competences.filter((competence) =>
+          competence.name
             .toLowerCase()
             .replace(/\s+/g, '')
             .includes(query.toLowerCase().replace(/\s+/g, ''))
@@ -46,7 +50,7 @@ export default function Home() {
             <div className="rounded-lg w-full relative">
               <Combobox.Input
                 className="input input-md bg-base-200 w-full"
-                displayValue={(person) => person.name}
+                displayValue={(competence: Competence) => competence.name}
                 onChange={(event) => setQuery(event.target.value)}
               />
               <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
@@ -61,20 +65,20 @@ export default function Home() {
               afterLeave={() => setQuery('')}
             >
               <Combobox.Options className="absolute mt-4 max-h-60 w-full overflow-auto rounded-md bg-base-200 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {filteredPeople.length === 0 && query !== '' ? (
+                {filteredSearch.length === 0 && query !== '' ? (
                   <div className="relative cursor-default select-none py-2 px-4 ">
                     Nothing found.
                   </div>
                 ) : (
-                  filteredPeople.map((person) => (
+                  filteredSearch.map((competence) => (
                     <Combobox.Option
-                      key={person.id}
+                      key={competence.id}
                       className={({ active }) =>
                         `relative cursor-default select-none py-2 pl-10 pr-4 ${
                           active ? 'bg-base-300' : ''
                         }`
                       }
-                      value={person}
+                      value={competence}
                     >
                       {({ selected, active }) => (
                         <>
@@ -83,7 +87,7 @@ export default function Home() {
                               selected ? 'font-medium' : 'font-normal'
                             }`}
                           >
-                            {person.name}
+                            {competence.name}
                           </span>
                           {selected ? (
                             <span
