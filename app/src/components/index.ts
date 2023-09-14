adae2800ee41cbf86870a8b3136337a2a4759858
bernardomrl <bernardomrl@icloud.com>
2023-09-13 23:20:16 -0300

    Adicionado Layout.

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as CustomToaster } from '@/components/CustomToaster';
 export { default as Header } from '@/components/Header';
+export { default as Layout } from '@/components/Layout';
